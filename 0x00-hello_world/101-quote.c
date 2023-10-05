14c2f32fb6ef63aa3fbc8c9c29c84fc1d1c14207
yonialex <yonasalemayehulemma0@gmail.com>
2023-10-05 16:39:06 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
   *main - Entry point
-  *Return: Always 0 (success)
+  *Return: Always 10 (success)
   */
 int main(void)
 {
-	Printf("and that piece of art is useful\" - Dora Korpar); 2015-10-19\n", 59);
-	return (0);
+	Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
