8946d4bcc85563d3a42db9d9e9cf52db98b569cb
yonialex <yonasalemayehulemma0@gmail.com>
2023-10-18 00:16:11 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,10 +5,8 @@
  * @n: The number to be checked
  * Return: 1 for postive num, -1 for negative num or zero for anything else
  */
- 
 int print_sign(int n)
-{       
-        
+{
 	if (n > 0)
 	{
 	_putchar(43);
